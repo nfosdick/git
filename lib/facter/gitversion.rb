9f558f1459427c5904e6b7908f871fdf5a29ab7f
root <root@puppet.lark.com>
2017-09-19 04:06:41 +0000

    testing

--- lib/facter/gitversion.rb
+++ lib/facter/gitversion.rb
@@ -1,6 +1,6 @@
 Facter.add(:gitversion) do
   confine :kernel => "Linux"
   setcode do
-    Facter::Util::Resolution.exec('/bin/git --version|awk "{print $3}"')
+    Facter::Util::Resolution.exec('/bin/git --version'.split(" ").last)
   end
 end
